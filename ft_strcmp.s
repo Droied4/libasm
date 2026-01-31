1712ac15706838737da5f2c34c9a9fd70a4b871a
Droied <droied.dev@gmail.com>
2026-01-31 16:21:34 +0100

    strcmp two implementations incomplete

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,13 +1,52 @@
-extern ft_strlen
-
 section .text
 	global ft_strcmp
 
 ft_strcmp:
 	xor rax, rax
 
+;Segunda version
+.loop: 
+	mov dl, byte [rsi + rax]
+	cmp [rdi + rax], dl
+	inc rax
+	;verificar si rdi sigue existiendo
+	je .loop
+
+.done:
+	;aqui guardar en rax la diferencia de rsi y rdi
+	;mov rax, rdi - rsi
+	ret
+
+;Primera version
+
 .loop:
-	cmp rdi, rsi
+	;rdi -> s1
+	;rsi -> s2
+	mov dl, byte [rsi + rax]
+	cmp [rdi + rax], dl
+	jg .great 
+	jl .less
+	inc rax
+	cmp [rdi + rax], 0
+	je .equal
+	cmp [rsi + rax], 0
+	je .equal
+	jmp .loop
+
+;hay que retornar la diferencia :/
+
+.great:
+	mov rax, 1
+	jmp.done
+
+.less:
+	mov rax, -1
+	jmp .done
+
+.equal:
+	mov rax, 0
+	jmp .done
+	ret
 
 .done:
 	ret
